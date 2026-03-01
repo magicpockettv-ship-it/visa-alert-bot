e4772e4893b7129258d00e498afd943215c77ee3
magicpockettv-ship-it <magicpockettv@gmail.com>
2026-03-02 02:47:42 +0530

    Delete visa_alert.py

--- visa_alert.py
+++
@@ -1,8 +0,0 @@
-from telegram import Bot
-
-BOT_TOKEN = "7983497912:AAF8IJ55S77pnlfdNyx9e8QcDeX1LC4PzP8"
-CHAT_ID = "5062141854"
-
-bot = Bot(token=BOT_TOKEN)
-
-bot.send_message(chat_id=CHAT_ID, text="Bot test successful ✅")
